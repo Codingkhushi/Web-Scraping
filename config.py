b8c8bb09932b71cd4176bc7f155a324604471544
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2025-06-28 10:27:28 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
+CSS_SELECTOR = "[class^='info-container']"
+# BASE_URL = "https://www.99acres.com/property-in-nashik-ffid" 
+# CSS_SELECTOR = "[class^='pageComponent undefined']"
+REQUIRED_KEYS = [
+    "name",
+    "price",
+    "location",
+    "capacity",
+    "rating",
+    "reviews",
+    "description",
+]
